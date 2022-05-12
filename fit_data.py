492e6b209b7caab4ff795119943b96e534ee337d
Thibault Lestang <thibault.lestang@protonmail.com>
2022-05-12 15:51:10 +0100

    Plot data using matplotlib

--- fit_data.py
+++ fit_data.py
@@ -1,4 +1,6 @@
 import numpy as np
+import matplotlib.pyplot as plt
 
 data = np.loadtxt("example_data.csv", delimiter=",", skiprows=1)
-                  
+plt.plot(data[:, 0], data[:, 1])
+plt.show()

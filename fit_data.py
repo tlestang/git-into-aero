6833fcec851077fd5dad3d8877dce0f1963c48b3
Thibault Lestang <thibault.lestang@protonmail.com>
2022-05-17 15:52:33 +0100

    Revert "plot raw data points as red disks"
    This reverts commit c3a997cf6430191a9647eb2576acf0d873961804.

--- fit_data.py
+++ fit_data.py
@@ -5,7 +5,7 @@ from model import model
 
 filename = "example_data.csv"
 data = np.loadtxt(filename, delimiter=",", skiprows=1)
-plt.plot(data[:, 0], data[:, 1], "ro")
+plt.plot(data[:, 0], data[:, 1])
 
 popt, pcov = curve_fit(model, data[:, 0], data[:, 1], p0=(1, 0.2))
 x = np.linspace(-2, 2.9, 100)
